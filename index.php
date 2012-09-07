b7480f7f66c93f68e4eb8c3ba16463ce3dc75b23
alkatim <alessio.albano@gmail.com>
2012-09-07 08:53:39 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <title>github</title>
 </head>
 <body>
-ora sto editando direttamente su git e poi faccio committ - 
-questa &egrave; la sola linea da committare - testing github - nuova release da committare - 7:44 AM</body>
+questa &egrave; la sola linea da committare - testing github - nuova release da committare - 7:44 AM
+
+ora sto editando direttamente su git e poi faccio committ - devo ricordare di fare pull poi per allineare il codice di git con quello in locale
+</body>
 </html>
